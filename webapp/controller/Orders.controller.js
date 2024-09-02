7f485df3ec562cbd09d4b28191d47648d3e5281b
Max Kei <mke@2bm.dk>
2024-09-02 19:39:21 +0000

    1stcommit

---
+++ webapp/controller/Orders.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("training.train01.controller.Orders", {
+        onInit: function () {
+
+        }
+    });
+});
